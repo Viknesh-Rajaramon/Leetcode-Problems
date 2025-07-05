68175cb665e25cecc5be6bdd4558c58351607d67
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-07-04 17:26:11 -0700

    Create 3307_Find_the_K-th_Character_in_String_Game_II.py

---
+++ Algorithms/Hard/3307_Find_the_K-th_Character_in_String_Game_II.py
@@ -0,0 +1,15 @@
+from imports import *
+
+class Solution:
+    def kthCharacter(self, k: int, operations: List[int]) -> str:
+        result = 0
+        while k != 1:
+            t = k.bit_length() - 1
+            if (1 << t) == k:
+                t -= 1
+
+            k -= 1 << t
+            if operations[t] == 1:
+                result += 1
+        
+        return chr(ord("a") + (result % 26))
