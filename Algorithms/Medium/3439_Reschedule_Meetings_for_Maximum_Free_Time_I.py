163c7a8e458952ac737e902ee9c698e9b82bf974
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-07-08 17:55:58 -0700

    Create 3439_Reschedule_Meetings_for_Maximum_Free_Time_I.py

---
+++ Algorithms/Medium/3439_Reschedule_Meetings_for_Maximum_Free_Time_I.py
@@ -0,0 +1,17 @@
+from imports import *
+
+class Solution:
+    def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
+        n = len(startTime)
+        result = 0
+        
+        curr_time = 0
+        for i in range(n):
+            curr_time += endTime[i] - startTime[i]
+            left = 0 if i < k else endTime[i-k]
+            right = eventTime if i == n-1 else startTime[i+1]
+            result = max(result, right - left - curr_time)
+            if i >= k-1:
+                curr_time -= endTime[i-k+1] - startTime[i-k+1]
+
+        return result
