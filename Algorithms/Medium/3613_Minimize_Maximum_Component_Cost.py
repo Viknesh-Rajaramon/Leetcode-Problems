f2764704b1a93120aa84c750b559aebbe3c3e275
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-07-14 17:06:36 -0700

    Create 3613_Minimize_Maximum_Component_Cost.py

---
+++ Algorithms/Medium/3613_Minimize_Maximum_Component_Cost.py
@@ -0,0 +1,31 @@
+from imports import *
+
+class Solution:
+    def minCost(self, n: int, edges: List[List[int]], k: int) -> int:
+        class UF:
+            def __init__(self, n: int):
+                self.parent = list(range(n))
+                self.components = n
+            
+            def find(self, x) -> int:
+                if x != self.parent[x]:
+                    self.parent[x] = self.find(self.parent[x])
+                
+                return self.parent[x]
+            
+            def union(self, x: int, y: int) -> None:
+                x, y = self.find(x), self.find(y)
+                
+                if x != y:
+                    self.parent[y] = x
+                    self.components -= 1
+        
+        uf = UF(n)
+        edges.sort(key = lambda x: x[2])
+
+        for u, v, w in edges:
+            uf.union(u, v)
+            if uf.components == k:
+                return w
+        
+        return 0
