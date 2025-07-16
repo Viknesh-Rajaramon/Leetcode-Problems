c86ed90fb5a41abfb25d450877f76fbbe1e62b9b
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-07-15 18:56:36 -0700

    Create 3201_Find_the_Maximum_Length_of_Valid_Subsequence_I.py

---
+++ Algorithms/Medium/3201_Find_the_Maximum_Length_of_Valid_Subsequence_I.py
@@ -0,0 +1,14 @@
+from imports import *
+
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        odd_p, odd_q, even_p, even_q = 0, 0, 0, 0
+        for num in nums:
+            if num % 2:
+                odd_p += 1
+                odd_q = 1 + even_q
+            else:
+                even_p += 1
+                even_q = 1 + odd_q
+
+        return max(odd_p, odd_q, even_p, even_q)
