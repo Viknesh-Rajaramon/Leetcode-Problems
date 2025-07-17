8cd3f97a23dce8c10759beae683e3be17f1bb3c9
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-07-16 21:28:17 -0700

    Create 3202_Find_the_Maximum_Length_of_Valid_Subsequence_II.py

---
+++ Algorithms/Medium/3202_Find_the_Maximum_Length_of_Valid_Subsequence_II.py
@@ -0,0 +1,11 @@
+from imports import *
+
+class Solution:
+    def maximumLength(self, nums: List[int], k: int) -> int:
+        dp = [[0] * k for _ in range(k)]
+        for curr in nums:
+            curr = curr % k
+            for prev in range(k):
+                dp[prev][curr] = dp[curr][prev] + 1
+        
+        return max(map(max, dp))
