816f07fb189668ba80f5cf6a33d27a937493fba5
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-07-24 23:32:23 -0700

    Create 849_Maximize_Distance_to_Closest_Person.py

---
+++ Algorithms/Medium/849_Maximize_Distance_to_Closest_Person.py
@@ -0,0 +1,18 @@
+from imports import *
+
+class Solution:
+    def maxDistToClosest(self, seats: List[int]) -> int:
+        n = len(seats)
+        
+        result, prev = 0, -1
+        for i in range(n):
+            if seats[i] == 1:
+                dist = i if prev == -1 else (i - prev) // 2
+                if dist > result:
+                    result = dist
+                prev = i
+        
+        if seats[n-1] == 0:
+            result = max(result, n-1-prev)
+        
+        return result
