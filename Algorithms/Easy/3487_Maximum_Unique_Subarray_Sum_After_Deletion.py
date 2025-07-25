9d29e8a6f3d787c9cca1bdb6592bb61a0b41d557
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-07-24 17:06:45 -0700

    Update 3487_Maximum_Unique_Subarray_Sum_After_Deletion.py

--- Algorithms/Easy/3487_Maximum_Unique_Subarray_Sum_After_Deletion.py
+++ Algorithms/Easy/3487_Maximum_Unique_Subarray_Sum_After_Deletion.py
@@ -2,10 +2,9 @@ from imports import *
 
 class Solution:
     def maxSum(self, nums: List[int]) -> int:
-        s = set()
-        curr_sum = 0
+        s, curr_sum = set(), 0
         for n in nums:
-            if curr_sum < curr_sum + n and n not in s:
+            if n > 0 and n not in s:
                 s.add(n)
                 curr_sum += n
 
