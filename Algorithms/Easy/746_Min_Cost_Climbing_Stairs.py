2fd4c878cb51d1d83cc7470596c90af420c27c44
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-07-26 11:36:28 -0700

    Update 746_Min_Cost_Climbing_Stairs.py

--- Algorithms/Easy/746_Min_Cost_Climbing_Stairs.py
+++ Algorithms/Easy/746_Min_Cost_Climbing_Stairs.py
@@ -3,11 +3,8 @@ from imports import *
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n = len(cost)
-        dp = [inf] * (n+1)
-        dp[n] = 0
-        dp[n-1] = cost[n-1]
-
-        for i in range(n-2, -1, -1):
-            dp[i] = cost[i] + min(dp[i+1], dp[i+2])
-        
-        return min(dp[0], dp[1])
+        dp = cost[ : 2] + [0] * (n - 2)
+        for i in range(2, n):
+            dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+            
+        return min(dp[n-1], dp[n-2])
