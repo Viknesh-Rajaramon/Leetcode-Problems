afc1b2dc99ba04a08c376136bc81c25c0767fbdc
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-07-26 17:36:53 -0700

    Create 2210_Count_Hills_and_Valleys_in_an_Array.py

---
+++ Algorithms/Easy/2210_Count_Hills_and_Valleys_in_an_Array.py
@@ -0,0 +1,13 @@
+from imports import *
+
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
+        result = 0
+        left = 0
+        for i in range(1, len(nums) - 1):
+            if nums[i] != nums[i+1]:
+                if (nums[i] > nums[i+1] and nums[i] > nums[left]) or (nums[i] < nums[i+1] and nums[i] < nums[left]):
+                    result += 1
+                left = i
+        
+        return result
