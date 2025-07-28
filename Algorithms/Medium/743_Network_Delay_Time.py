b9664baa5bae1fc63614eb8395d858e02e336949
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-07-28 12:24:43 -0700

    Create 743_Network_Delay_Time.py

---
+++ Algorithms/Medium/743_Network_Delay_Time.py
@@ -0,0 +1,25 @@
+from imports import *
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        edges = defaultdict(dict)
+        for u, v, w in times:
+            edges[u][v] = w
+        
+        dist = {node: inf for node in range(1, n + 1)}
+        dist[k] = 0
+
+        heap = [(0, k)]
+        while heap:
+            w, u = heappop(heap)
+            if w > dist[u]:
+                continue
+            
+            for v, t in edges[u].items():
+                new_w = t + w
+                if new_w < dist[v]:
+                    dist[v] = new_w
+                    heappush(heap, (new_w, v))
+        
+        max_ = max(dist.values())
+        return max_ if max_ < inf else -1
