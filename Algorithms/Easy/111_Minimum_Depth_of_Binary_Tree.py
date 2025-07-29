81e7e757678e24d610e1432df1eb874a95c006ba
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-07-29 09:45:58 -0700

    Update 111_Minimum_Depth_of_Binary_Tree.py

--- Algorithms/Easy/111_Minimum_Depth_of_Binary_Tree.py
+++ Algorithms/Easy/111_Minimum_Depth_of_Binary_Tree.py
@@ -2,16 +2,17 @@ from imports import *
 
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        def dfs(node: Optional[TreeNode]) -> int:
-            if not node:
-                return inf
+        if not root:
+            return 0
+        
+        queue = deque([(root, 1)])
+        while queue:
+            node, depth = queue.popleft()
+            if not node.left and not node.right:
+                return depth
             
-            left_depth = dfs(node.left)
-            right_depth = dfs(node.right)
-
-            if left_depth == inf and right_depth == inf:
-                return 1
+            if node.left:
+                queue.append((node.left, depth + 1))
             
-            return min(left_depth, right_depth) + 1
-
-        return dfs(root) if root else 0
+            if node.right:
+                queue.append((node.right, depth + 1))
