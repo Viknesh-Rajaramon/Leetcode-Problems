da2d3b9995b7d61b6361222fbdc47284da16a61b
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-07-28 17:56:09 -0700

    Create 2411_Smallest_Subarrays_With_Maximum_Bitwise_OR.py

---
+++ Algorithms/Medium/2411_Smallest_Subarrays_With_Maximum_Bitwise_OR.py
@@ -0,0 +1,15 @@
+from imports import *
+
+class Solution:
+    def smallestSubarrays(self, nums: List[int]) -> List[int]:
+        n = len(nums)        
+        
+        result = [1] * n
+        for i in range(n):
+            j = i - 1
+            while j >= 0 and nums[j] | nums[i] != nums[j]:
+                result[j] = i - j + 1
+                nums[j] |= nums[i]
+                j -= 1
+
+        return result
