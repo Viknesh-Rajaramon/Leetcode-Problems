a52fec07d0bc90e913a4de17ab95c88b6a71de9b
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-07-29 17:51:10 -0700

    Create 2419_Longest_Subarray_With_Maximum_Bitwise_AND.py

---
+++ Algorithms/Medium/2419_Longest_Subarray_With_Maximum_Bitwise_AND.py
@@ -0,0 +1,15 @@
+from imports import *
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        max_num, length, max_length = max(nums), 0, 0
+        for num in nums:
+            if num == max_num:
+                length += 1
+            else:
+                if length > max_length:
+                    max_length = length
+                
+                length = 0
+
+        return max_length if max_length > length else length
