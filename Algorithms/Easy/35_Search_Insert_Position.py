c171347fee094a2fa603b67ec766082962d60ac8
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-07-29 19:35:33 -0700

    Update 35_Search_Insert_Position.py

--- Algorithms/Easy/35_Search_Insert_Position.py
+++ Algorithms/Easy/35_Search_Insert_Position.py
@@ -2,19 +2,4 @@ from imports import *
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        low = 0
-        high = len(nums)-1
-        while low <= high:
-            mid = low + (high - low) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid-1] < target and target < nums[mid]:
-                return mid
-            elif nums[mid] < target and mid+1 < len(nums) and target < nums[mid+1]:
-                return mid+1
-            elif target < nums[mid]:
-                high = mid - 1
-            elif nums[mid] < target:
-                low = mid + 1
-        
-        return low
+        return bisect_left(nums, target)
