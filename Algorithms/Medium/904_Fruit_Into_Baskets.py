b3bd73e47f4d76f7b19c6e6c5a8fdc40fedb4b85
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-08-04 18:04:16 -0700

    Create 904_Fruit_Into_Baskets.py

---
+++ Algorithms/Medium/904_Fruit_Into_Baskets.py
@@ -0,0 +1,22 @@
+from imports import *
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        result = 0
+
+        fruit_1, fruit_2 = -1, -1
+        count_1, count_2, prev = 0, 0, 0
+
+        for f in fruits:
+            if f == fruit_1:
+                count_1 += 1
+                prev += 1
+            elif f == fruit_2:
+                fruit_1, fruit_2 = fruit_2, fruit_1
+                count_1, count_2, prev = count_2 + 1, count_1, 1
+            else:
+                result = max(result, count_1 + count_2)
+                fruit_1, fruit_2 = f, fruit_1
+                count_1, count_2, prev = 1, prev, 1
+
+        return max(result, count_1 + count_2)
