3c25dc661d37b4b687949b90d6e75b27fe123f8d
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-08-04 18:05:17 -0700

    Update 3477_Fruits_Into_Baskets_II.py

--- Algorithms/Easy/3477_Fruits_Into_Baskets_II.py
+++ Algorithms/Easy/3477_Fruits_Into_Baskets_II.py
@@ -3,12 +3,12 @@ from imports import *
 class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
         result = 0
-        n, m = len(fruits), len(baskets)
-        for i in range(n):
-            for j in range(m):
-                if baskets[j] >= fruits[i]:
-                    baskets[j] = 0
+
+        for f in fruits:
+            for b in baskets:
+                if b >= f:
+                    baskets.remove(b)
                     result += 1
                     break
         
-        return n - result
+        return len(fruits) - result
