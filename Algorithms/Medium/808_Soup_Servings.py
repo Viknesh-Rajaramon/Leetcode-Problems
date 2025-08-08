9a0fb4943b0e439dacf634ad9ae9966ee1136097
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-08-07 19:30:15 -0700

    Create 808_Soup_Servings.py

---
+++ Algorithms/Medium/808_Soup_Servings.py
@@ -0,0 +1,23 @@
+from imports import *
+
+class Solution:
+    def soupServings(self, n: int) -> float:
+        if n > 5000:
+            return 1.0
+
+        m = ceil(n / 25)
+        
+        @lru_cache(None)
+        def calc_probability(A: int, B: int) -> float:
+            if A <= 0 and B <= 0:
+                return 0.5
+            
+            if A <= 0:
+                return 1.0
+            
+            if B <= 0:
+                return 0.0
+            
+            return 0.25 * (calc_probability(A-4, B) + calc_probability(A-3, B-1) + calc_probability(A-2, B-2) + calc_probability(A-1, B-3))
+
+        return calc_probability(m, m)
