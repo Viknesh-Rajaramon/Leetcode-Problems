2a93fda941095f98466f4eb9d2b6ab4aa9889b26
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-08-09 09:55:09 -0700

    Create 1185_Day_of_the_Week.py

---
+++ Algorithms/Easy/1185_Day_of_the_Week.py
@@ -0,0 +1,6 @@
+from imports import *
+
+class Solution:
+    def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
+        days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        return days[datetime.date(year, month, day).weekday()]
