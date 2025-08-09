ce8db3142f211a14c6a3370cbc3ad0784e18e01f
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-08-08 22:38:06 -0700

    Update 796_Rotate_String.py

--- Algorithms/Easy/796_Rotate_String.py
+++ Algorithms/Easy/796_Rotate_String.py
@@ -1,16 +1,7 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        n = len(s)
-        if len(goal) != n:
-            return False
-        
-        count = n
-        new_s = s
-        while count > 0:
-            if new_s == goal:
+        for i in range(len(s)):
+            if s[i : ] + s[ : i] == goal:
                 return True
             
-            new_s = new_s[1 : ] + new_s[0]
-            count -= 1
-        
         return False
