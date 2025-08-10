d202c85b06cbb36710eb08d7b2664fcf3b40ffc2
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-08-09 17:31:11 -0700

    Create 869_Reordered_Power_of_2.py

---
+++ Algorithms/Medium/869_Reordered_Power_of_2.py
@@ -0,0 +1,9 @@
+from imports import *
+
+nums = []
+for i in range(int(log2(10**9)) + 1):
+    nums.append(sorted(str(2**i)))
+
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        return sorted(str(n)) in nums
