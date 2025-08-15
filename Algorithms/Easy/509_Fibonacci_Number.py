b7a4742ea486a8da802ac3aad232be7711fd8d0c
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-08-15 09:28:55 -0700

    Update 509_Fibonacci_Number.py

--- Algorithms/Easy/509_Fibonacci_Number.py
+++ Algorithms/Easy/509_Fibonacci_Number.py
@@ -1,13 +1,9 @@
+dp = {0: 0, 1: 1}
+
 class Solution:
     def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
-        
-        dp = [-1] * (n+1)
-        dp[0] = 0
-        dp[1] = 1
-
-        for i in range(2, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-
+        if n in dp:
+            return dp[n]
+            
+        dp[n] = self.fib(n-2) + self.fib(n-1)
         return dp[n]
