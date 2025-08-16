bcf4a917c4ebdd01bafabe52898037d45201dcf7
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-08-15 17:17:36 -0700

    Update 1323_Maximum_69_Number.py

--- Algorithms/Easy/1323_Maximum_69_Number.py
+++ Algorithms/Easy/1323_Maximum_69_Number.py
@@ -1,10 +1,8 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
         n = str(num)
-        result = list(n)
         idx = n.find("6")
         if idx == -1:
             return num
         
-        result[idx] = "9"
-        return int("".join(result))
+        return num + 3 * 10**(len(n) - 1 - idx)
