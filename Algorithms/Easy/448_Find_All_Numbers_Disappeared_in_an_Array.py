ad415e22941dccca321d12c767ceb2cbb9f4310e
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-08-18 23:27:25 -0700

    Update 448_Find_All_Numbers_Disappeared_in_an_Array.py

--- Algorithms/Easy/448_Find_All_Numbers_Disappeared_in_an_Array.py
+++ Algorithms/Easy/448_Find_All_Numbers_Disappeared_in_an_Array.py
@@ -3,13 +3,13 @@ from imports import *
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        hash_table = [0] * (n + 1)
+        hash_table = [0] * (n+1)
         for num in nums:
             hash_table[num] = 1
         
-        ans = []
-        for i in range(1, n+1):
-            if hash_table[i] == 0:
-                ans.append(i)
+        result = []
+        for num in range(1, n+1):
+            if hash_table[num] == 0:
+                result.append(num)
         
-        return ans
+        return result
