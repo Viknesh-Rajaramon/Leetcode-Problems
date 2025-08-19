d71953aad804d544646c5702625a825b877f5410
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-08-18 17:16:50 -0700

    Create 2348_Number_of_Zero-Filled_Subarrays.py

---
+++ Algorithms/Medium/2348_Number_of_Zero-Filled_Subarrays.py
@@ -0,0 +1,18 @@
+from imports import *
+
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        result = 0
+        
+        length = 0
+        for num in nums:
+            if num == 0:
+                length += 1
+            elif length > 0:
+                result += length * (length+1) // 2
+                length = 0
+
+        if length > 0:
+            result += length * (length+1) // 2
+        
+        return result
