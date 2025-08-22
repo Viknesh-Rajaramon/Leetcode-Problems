daf9e6ab3c62289be942ec336e0b4fc29579ec4d
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-08-21 22:08:03 -0700

    Create 1504_Count_Submatrices_With_All_Ones.py

---
+++ Algorithms/Medium/1504_Count_Submatrices_With_All_Ones.py
@@ -0,0 +1,32 @@
+from imports import *
+
+class Solution:
+    def numSubmat(self, mat: List[List[int]]) -> int:
+        m, n = len(mat), len(mat[0])
+        def count_rows(heights: List[int]) -> int:
+            stack, sub_matrices = [], [0] * n
+            for i in range(n):
+                while stack and heights[stack[-1]] >= heights[i]:
+                    stack.pop()
+                
+                if stack:
+                    l = stack[-1]
+                    sub_matrices[i] = sub_matrices[l] + heights[i] * (i-l)
+                else:
+                    sub_matrices[i] = heights[i] * (i+1)
+        
+                stack.append(i)
+            
+            return sum(sub_matrices)
+
+        result, heights = 0, [0] * n
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1:
+                    heights[j] += 1
+                else:
+                    heights[j] = 0
+                    
+            result += count_rows(heights)
+        
+        return result
