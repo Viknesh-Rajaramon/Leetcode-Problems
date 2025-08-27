b7492ca5a04b059ad6f76278252e07de626e1546
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-08-26 17:43:00 -0700

    Create 3459_Length_of_Longest_V-Shaped_Diagonal_Segment.py

---
+++ Algorithms/Hard/3459_Length_of_Longest_V-Shaped_Diagonal_Segment.py
@@ -0,0 +1,30 @@
+from imports import *
+
+class Solution:
+    def lenOfVDiagonal(self, grid: List[List[int]]) -> int:
+        n, m = len(grid), len(grid[0])
+        directions = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
+        expected_value = [2, 2, 0]
+
+        @lru_cache(None)
+        def dfs(cx: int, cy: int, d: int, turned: bool) -> int:
+            nx, ny = cx + directions[d][0], cy + directions[d][1]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m or grid[nx][ny] != expected_value[grid[cx][cy]]:
+                return 1
+            
+            max_step = dfs(nx, ny, d, turned) + 1
+            if not turned:
+                max_step = max(max_step, dfs(nx, ny, (d+1)%4, True) + 1)
+            
+            return max_step
+        
+        result = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:
+                    tm = (n - i, j + 1, i + 1, m - j)
+                    for d in range(4):
+                        if tm[d] > result:
+                            result = max(result, dfs(i, j, d, False))
+        
+        return result
