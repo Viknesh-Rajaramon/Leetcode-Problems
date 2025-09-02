6f627aa587a6b6705eff701e785ca8da491f0a56
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-09-01 17:20:09 -0700

    Create 3025_Find_the_Number_of_Ways_to_Place_People_I.py

---
+++ Algorithms/Medium/3025_Find_the_Number_of_Ways_to_Place_People_I.py
@@ -0,0 +1,18 @@
+from imports import *
+
+class Solution:
+    def numberOfPairs(self, points: List[List[int]]) -> int:
+        n = len(points)
+        points.sort(key = lambda x: (x[0], -x[1]))
+        
+        result = 0
+        for i in range(n-1):
+            bottom = -1
+            for j in range(i+1, n):
+                if bottom < points[j][1] <= points[i][1]:
+                    result += 1
+                    bottom = points[j][1]
+                    if bottom == points[i][1]:
+                        break
+                        
+        return result
