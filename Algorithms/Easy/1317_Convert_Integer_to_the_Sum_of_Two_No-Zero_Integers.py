e61309ed1d7ff8138ca44f184915b6595a098b74
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-09-07 17:08:08 -0700

    Create 1317_Convert_Integer_to_the_Sum_of_Two_No-Zero_Integers.py

---
+++ Algorithms/Easy/1317_Convert_Integer_to_the_Sum_of_Two_No-Zero_Integers.py
@@ -0,0 +1,18 @@
+from imports import *
+
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        def check(num: int) -> bool:
+            while num:
+                if num % 10 == 0:
+                    return False
+                
+                num //= 10
+            
+            return True
+
+        for a in range(1, n):
+            if check(a) and check(n-a):
+                return [a, n-a]
+        
+        return [1, n-1]
