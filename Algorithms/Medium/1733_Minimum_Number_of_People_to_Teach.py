bac10b44253f2d87291325f10e2c63638bbb4316
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-09-09 18:29:39 -0700

    Create 1733_Minimum_Number_of_People_to_Teach.py

---
+++ Algorithms/Medium/1733_Minimum_Number_of_People_to_Teach.py
@@ -0,0 +1,19 @@
+from imports import *
+
+class Solution:
+    def minimumTeachings(self, n: int, languages: List[List[int]], friendships: List[List[int]]) -> int:
+        learned, users, known = list(map(set, languages)), set(), defaultdict(int)
+        def can_communicate(x: int):
+            if x in users:
+                return
+            
+            users.add(x)
+            for lang in languages[x]:
+                known[lang] += 1
+
+        for (u, v) in friendships:
+            if learned[u-1].isdisjoint(learned[v-1]):
+                can_communicate(u-1)
+                can_communicate(v-1)
+        
+        return len(users) - max(known.values(), default=0)
