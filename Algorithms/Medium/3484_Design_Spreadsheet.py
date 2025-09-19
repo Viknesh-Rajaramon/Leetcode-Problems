527e851fec1d12f789dc7be1d1489b5c53c7ccc2
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-09-18 17:36:30 -0700

    Update 3484_Design_Spreadsheet.py

--- Algorithms/Medium/3484_Design_Spreadsheet.py
+++ Algorithms/Medium/3484_Design_Spreadsheet.py
@@ -3,7 +3,7 @@ from imports import *
 class Spreadsheet:
 
     def __init__(self, rows: int):
-        self.spreadsheet = defaultdict(int)
+        self.spreadsheet = {}
 
     def setCell(self, cell: str, value: int) -> None:
         self.spreadsheet[cell] = value
@@ -11,15 +11,26 @@ class Spreadsheet:
     def resetCell(self, cell: str) -> None:
         self.spreadsheet[cell] = 0
 
-    def _getIntValue(self, X: str) -> int:
-        if ord("A") <= ord(X[0]) <= ord("Z"):
-            return self.spreadsheet[X]
-        
-        return int(X)
-
     def getValue(self, formula: str) -> int:
-        X, Y = formula[1:].split("+")
-        return self._getIntValue(X) + self._getIntValue(Y)
+        x, y = formula[1:].split("+")
+
+        if x in self.spreadsheet:
+            x_val = self.spreadsheet[x]
+        else:
+            if x.isdigit():
+                x_val = int(x)
+            else:
+                x_val = 0
+
+        if y in self.spreadsheet:
+            y_val = self.spreadsheet[y]
+        else:
+            if y.isdigit():
+                y_val = int(y)
+            else:
+                y_val = 0
+
+        return x_val + y_val
 
 
 # Your Spreadsheet object will be instantiated and called as such:
