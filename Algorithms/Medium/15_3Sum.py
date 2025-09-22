bc4c94424d8251de59291af59d0a0c0025ccd4ae
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-09-21 18:18:32 -0700

    Update 15_3Sum.py

--- Algorithms/Medium/15_3Sum.py
+++ Algorithms/Medium/15_3Sum.py
@@ -6,23 +6,27 @@ class Solution:
         result = []
         nums.sort()
 
-        for i in range(n):
+        for i in range(n-2):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             
-            j = i + 1
-            k = n - 1
-
+            j, k = i+1, n-1
             while j < k:
-                if nums[j] + nums[k] < -nums[i]:
-                    j += 1
-                elif nums[j] + nums[k] > -nums[i]:
-                    k -= 1
-                else:
+                total = nums[i] + nums[j] + nums[k]
+                if total == 0:
                     result.append([nums[i], nums[j], nums[k]])
-                    j += 1
-
-                    while nums[j] == nums[j-1] and j < k:
+                    
+                    while j < k and nums[j] == nums[j+1]:
                         j += 1
-        
+                    
+                    while j < k and nums[k] == nums[k-1]:
+                        k -= 1
+                    
+                    j += 1
+                    k -= 1
+                elif total < 0:
+                    j += 1
+                else:
+                    k -= 1
+
         return result
