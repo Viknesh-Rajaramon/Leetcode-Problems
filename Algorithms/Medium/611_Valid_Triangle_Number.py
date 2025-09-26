5f9a8c2ffe353a5f24bda13bb296b7a4e86990be
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-09-25 17:21:23 -0700

    Create 611_Valid_Triangle_Number.py

---
+++ Algorithms/Medium/611_Valid_Triangle_Number.py
@@ -0,0 +1,23 @@
+from imports import *
+
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        n, result = len(nums), 0
+        for i in range(n-1, 1, -1):
+            if nums[0] + nums[1] > nums[i]:
+                result += (i+1) * i * (i-1) // 6
+                break
+            
+            if nums[i-2] + nums[i-1] < nums[i]:
+                continue
+            
+            left, right = 0, i-1
+            while left < right:
+                if nums[left] + nums[right] > nums[i]:
+                    result += right - left
+                    right -= 1
+                else:
+                    left += 1
+
+        return result
