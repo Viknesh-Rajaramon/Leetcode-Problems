9e19eadc5d55801100d8934444a0b1cac106ae33
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-09-30 23:08:05 -0700

    Create 77_Combinations.py

---
+++ Algorithms/Medium/77_Combinations.py
@@ -0,0 +1,17 @@
+from imports import *
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result = []
+        def combinations(i: int, nums: List[int]):
+            if len(nums) == k:
+                result.append(nums.copy())
+                return
+            
+            for j in range(i+1, n+1):
+                nums.append(j)
+                combinations(j, nums)
+                nums.pop()
+        
+        combinations(0, [])
+        return result
