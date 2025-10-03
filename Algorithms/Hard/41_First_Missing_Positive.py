c9901f19fe51af519e3ed510e2abb6cb9418a549
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-10-02 21:23:12 -0700

    Create 41_First_Missing_Positive.py

---
+++ Algorithms/Hard/41_First_Missing_Positive.py
@@ -0,0 +1,10 @@
+from imports import *
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        max_, nums = 2**31, set(nums)
+        for i in range(1, max_):
+            if i not in nums:
+                return i
+
+        return max_
