ab3f2600ef412c0ce9bd9ddee057940e01851ad8
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-10-08 17:09:52 -0700

    Create 1488_Avoid_Flood_in_The_City.py

---
+++ Algorithms/Medium/1488_Avoid_Flood_in_The_City.py
@@ -0,0 +1,25 @@
+from imports import *
+
+class Solution:
+    def avoidFlood(self, rains: List[int]) -> List[int]:
+        last, queue = {}, deque()
+        
+        result = []
+        for i, lake in enumerate(rains):
+            if lake:
+                if lake in last:
+                    for j in queue:
+                        if j > last[lake]:
+                            result[j] = lake
+                            queue.remove(j)
+                            break
+                    else:
+                        return []
+
+                result.append(-1)
+                last[lake] = i
+            else:
+                result.append(1)
+                queue.append(i)
+        
+        return result
