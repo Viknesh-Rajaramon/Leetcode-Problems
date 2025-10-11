4ff833cb487c30d5e675899afd6da5f5cd813f09
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-10-11 11:33:57 -0700

    Create 3147_Taking_Maximum_Energy_From_the_Mystic_Dungeon.py

---
+++ Algorithms/Medium/3147_Taking_Maximum_Energy_From_the_Mystic_Dungeon.py
@@ -0,0 +1,12 @@
+from imports import *
+
+class Solution:
+    def maximumEnergy(self, energy: List[int], k: int) -> int:
+        n = len(energy)
+        result = max(energy[n-k : ])
+        for i in range(n-1-k, -1, -1):
+            energy[i] += energy[i+k]
+            if result < energy[i]:
+                result = energy[i]
+        
+        return result
