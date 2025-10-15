e6d1650a0c6a2a4ede60a6037e38d6b3946cad76
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-10-14 18:47:44 -0700

    Create 3349_Adjacent_Increasing_Subarrays_Detection_I.py

---
+++ Algorithms/Easy/3349_Adjacent_Increasing_Subarrays_Detection_I.py
@@ -0,0 +1,18 @@
+from imports import *
+
+class Solution:
+    def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
+        prev, prev_streak, curr_streak = -inf, 0, 0
+        for num in nums:
+            if num > prev:
+                curr_streak += 1
+            else:
+                prev_streak = curr_streak
+                curr_streak = 1
+            
+            if curr_streak >= 2*k or (prev_streak >= k and curr_streak >= k):
+                return True
+
+            prev = num
+        
+        return False
