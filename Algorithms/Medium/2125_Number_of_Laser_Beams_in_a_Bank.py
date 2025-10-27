7562268f42eca1c819f62672a95927bca7f1738e
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-10-26 17:04:10 -0700

    Update 2125_Number_of_Laser_Beams_in_a_Bank.py

--- Algorithms/Medium/2125_Number_of_Laser_Beams_in_a_Bank.py
+++ Algorithms/Medium/2125_Number_of_Laser_Beams_in_a_Bank.py
@@ -5,9 +5,10 @@ class Solution:
         result, prev_count = 0, 0
         for lasers in bank:
             curr_count = lasers.count("1")
+            if not curr_count:
+                continue
             
-            if curr_count > 0:
-                result += prev_count * curr_count
-                prev_count = curr_count
+            result += prev_count * curr_count
+            prev_count = curr_count
         
         return result
