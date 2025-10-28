f822fafc5055e5bbbe83b71136cbc8d4ae8e29c8
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-10-27 17:49:50 -0700

    Create 3354_Make_Array_Elements_Equal_to_Zero.py

---
+++ Algorithms/Easy/3354_Make_Array_Elements_Equal_to_Zero.py
@@ -0,0 +1,16 @@
+from imports import *
+
+class Solution:
+    def countValidSelections(self, nums: List[int]) -> int:
+        result, left, right = 0, 0, sum(nums)
+        for num in nums:
+            if num == 0:
+                if left == right:
+                    result += 2
+                elif abs(left - right) == 1:
+                    result += 1
+            else:
+                left += num
+                right -= num
+
+        return result
