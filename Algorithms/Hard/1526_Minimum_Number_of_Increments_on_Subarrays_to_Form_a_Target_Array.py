530037a1e0115f5a2c46085d44039a037ed47065
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-10-29 17:06:48 -0700

    Create 1526_Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array.py

---
+++ Algorithms/Hard/1526_Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array.py
@@ -0,0 +1,12 @@
+from imports import *
+
+class Solution:
+    def minNumberOperations(self, target: List[int]) -> int:
+        result, prev = 0, 0
+        for num in target:
+            if num > prev:
+                result += num - prev
+            
+            prev = num
+        
+        return result
