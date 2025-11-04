e83ef58551fc5b88a908079a8a0a02b2d62166af
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-11-03 17:59:31 -0700

    Create 3318_Find_X-Sum_of_All_K-Long_Subarrays_I.py

---
+++ Algorithms/Easy/3318_Find_X-Sum_of_All_K-Long_Subarrays_I.py
@@ -0,0 +1,26 @@
+from imports import *
+
+class Solution:
+    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
+        result, n, freq = [], len(nums), {}
+        for i in range(k):
+            if nums[i] not in freq:
+                freq[nums[i]] = 1
+            else:
+                freq[nums[i]] += 1
+
+        for i in range(n-k+1):
+            top = sorted(freq.items(), key = lambda p: (p[1], p[0]), reverse = True)
+            result.append(sum(val * f for val, f in top[:x]))
+
+            freq[nums[i]] -= 1
+            if freq[nums[i]] == 0:
+                del freq[nums[i]]
+            
+            if i + k < n:
+                if nums[i+k] not in freq:
+                    freq[nums[i+k]] = 1
+                else:
+                    freq[nums[i+k]] += 1
+
+        return result
