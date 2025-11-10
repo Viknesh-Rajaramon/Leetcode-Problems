dd308ad86e8b2ef825eace6a17ce6632424f4a54
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-11-09 18:01:30 -0700

    Create 3542_Minimum_Operations_to_Convert_All_Elements_to_Zero.py

---
+++ Algorithms/Medium/3542_Minimum_Operations_to_Convert_All_Elements_to_Zero.py
@@ -0,0 +1,15 @@
+from imports import *
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        result, stack = 0, [0]
+        for num in nums:
+            while stack[-1] > num:
+                stack.pop()
+            
+            if num > stack[-1]:
+                result += 1
+            
+            stack.append(num)
+
+        return result
