3916226d22ad6539cc26d89330142e2f050fb021
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-11-19 17:34:54 -0700

    Create 757_Set_Intersection_Size_At_Least_Two.py

---
+++ Algorithms/Hard/757_Set_Intersection_Size_At_Least_Two.py
@@ -0,0 +1,15 @@
+from imports import *
+
+class Solution:
+    def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key = lambda x: (x[1], -x[0]))
+        result, prev_start, prev_end = 0, -inf, -inf
+        for start, end in intervals:
+            if start > prev_end:
+                result += 2
+                prev_start, prev_end = end-1, end
+            elif start > prev_start:
+                result += 1
+                prev_start, prev_end = prev_end, end
+
+        return result
