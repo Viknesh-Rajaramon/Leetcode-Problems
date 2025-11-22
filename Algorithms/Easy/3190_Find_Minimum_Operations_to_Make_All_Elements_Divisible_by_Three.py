8438786398b97f374afaf09233a2e92518572510
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-11-21 17:23:43 -0700

    Update 3190_Find_Minimum_Operations_to_Make_All_Elements_Divisible_by_Three.py

--- Algorithms/Easy/3190_Find_Minimum_Operations_to_Make_All_Elements_Divisible_by_Three.py
+++ Algorithms/Easy/3190_Find_Minimum_Operations_to_Make_All_Elements_Divisible_by_Three.py
@@ -2,10 +2,9 @@ from imports import *
 
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        min_ops = 0
-        n = len(nums)
-        for i in range(n):
-            x = nums[i] % 3
-            min_ops += min(x, 3 - x)
+        result = 0
+        for num in nums:
+            x = num % 3
+            result += min(x, 3 - x)
         
-        return min_ops
+        return result
