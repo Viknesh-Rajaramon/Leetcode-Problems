b607b9eebf545ca5defa9e70cd9caacf1381b1eb
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-11-23 17:10:01 -0700

    Create 1018_Binary_Prefix_Divisible_By_5.py

---
+++ Algorithms/Easy/1018_Binary_Prefix_Divisible_By_5.py
@@ -0,0 +1,10 @@
+from imports import *
+
+class Solution:
+    def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
+        result, rem = [], 0
+        for i in nums:
+            rem = (rem*2 + i) % 5
+            result.append(rem == 0)
+
+        return result
