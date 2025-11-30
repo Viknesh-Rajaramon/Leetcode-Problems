047ddefcf4d562cff6891c3e8146af5bda5d74c0
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-11-29 18:06:39 -0700

    Create 1590_Make_Sum_Divisible_by_P.py

---
+++ Algorithms/Medium/1590_Make_Sum_Divisible_by_P.py
@@ -0,0 +1,18 @@
+from imports import *
+
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        target = sum(nums) % p
+        if target == 0:
+            return 0
+        
+        mod_map, curr_sum, result = {0: -1}, 0, inf
+        for i, num in enumerate(nums):
+            curr_sum = (curr_sum + num) % p
+            needed = (curr_sum - target + p) % p
+            if needed in mod_map:
+                result = min(result, i - mod_map[needed])
+            
+            mod_map[curr_sum] = i
+        
+        return -1 if result == len(nums) else result
