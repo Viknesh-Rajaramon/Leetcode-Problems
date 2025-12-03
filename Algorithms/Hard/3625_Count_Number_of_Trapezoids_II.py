2a3b4f502005b3f59070d14c17d728e599bc9f52
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-12-02 18:09:52 -0700

    Update 3625_Count_Number_of_Trapezoids_II.py

--- Algorithms/Hard/3625_Count_Number_of_Trapezoids_II.py
+++ Algorithms/Hard/3625_Count_Number_of_Trapezoids_II.py
@@ -2,38 +2,39 @@ from imports import *
 
 class Solution:
     def countTrapezoids(self, points: List[List[int]]) -> int:
-        n = len(points)
-
-        slopes = defaultdict(lambda: defaultdict(int))
-        mids = defaultdict(lambda: defaultdict(int)) 
-
-        def slope(p1: Tuple[int, int], p2: Tuple[int, int]) -> float:
-            dx, dy = p2[0] - p1[0], p2[1] - p1[1]
-            g = gcd(dx, dy)
-            dx //= g
-            dy //= g
-            
-            if dx < 0 or (dx == 0 and dy < 0):
-                dx, dy = -dx, -dy
-            
-            return (dy, dx)
-
+        n, slopes, mids = len(points), defaultdict(list), defaultdict(list)
         for i in range(n):
             x1, y1 = points[i]
             for j in range(i+1, n):
                 x2, y2 = points[j]
-                s = slope(points[i], points[j])
-                slopes[s][s[1] * y1 - s[0] * x1] += 1
-                mids[(x1 + x2, y1 + y2)][s] += 1
+                dx, dy = x1 - x2, y1 - y2
+
+                if x1 == x2:
+                    k, b = inf, x1
+                else:
+                    k, b = (y2 - y1) / (x2 - x1), (y1 * dx - x1 * dy) / dx
+                
+                mid = (x1 + x2) * 10000 + (y1 + y2)
+                slopes[k].append(b)
+                mids[mid].append(k)
         
         result = 0
-        for line_counts in slopes.values():
-            total = sum(line_counts.values())
-            result += (total**2 - sum(v**2 for v in line_counts.values())) // 2
-        
-        for sCnt in mids.values():
-            v = sum(sCnt.values())
-            if v > 1:
-                result -= v * (v-1) // 2 - sum(c * (c-1) // 2 for c in sCnt.values())
+        for s in slopes.values():
+            if len(s) == 1:
+                continue
+            
+            total_sum = 0
+            for c in Counter(s).values():
+                result += total_sum * c
+                total_sum += c
+            
+        for m in mids.values():
+            if len(m) == 1:
+                continue
+            
+            total_sum = 0
+            for c in Counter(m).values():
+                result -= total_sum * c
+                total_sum += c
 
         return result
