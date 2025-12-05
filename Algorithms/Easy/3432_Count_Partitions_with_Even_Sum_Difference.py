69c0330c15455ea18ffd5cccb4ba90381e370f3d
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-12-04 17:37:02 -0700

    Update 3432_Count_Partitions_with_Even_Sum_Difference.py

--- Algorithms/Easy/3432_Count_Partitions_with_Even_Sum_Difference.py
+++ Algorithms/Easy/3432_Count_Partitions_with_Even_Sum_Difference.py
@@ -2,13 +2,4 @@ from imports import *
 
 class Solution:
     def countPartitions(self, nums: List[int]) -> int:
-        result = 0
-        curr_sum, total_sum = 0, sum(nums)
-        for i in range(len(nums)-1):
-            curr_sum += nums[i]
-            total_sum -= nums[i]
-            
-            if abs(total_sum - curr_sum) % 2 == 0:
-                result += 1
-        
-        return result
+        return len(nums) - 1 if sum(nums) % 2 == 0 else 0
