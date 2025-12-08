fe54044fa4aeab7643f801b72edbb628cd86b1ba
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-12-07 17:16:13 -0700

    Create 1925_Count_Square_Sum_Triples.py

---
+++ Algorithms/Easy/1925_Count_Square_Sum_Triples.py
@@ -0,0 +1,12 @@
+from imports import *
+
+class Solution:
+    def countTriples(self, n: int) -> int:
+        result = 0
+        for a in range(1, n):
+            for b in range(a+1, n):
+                c = sqrt(a**2 + b**2)
+                if int(c) == c and c <= n:
+                    result += 2
+
+        return result
