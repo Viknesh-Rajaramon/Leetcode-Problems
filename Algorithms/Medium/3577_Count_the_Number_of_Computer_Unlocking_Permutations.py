5495c11d78aa0083f125de43ae35752ffde89568
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-12-09 17:21:13 -0700

    Update 3577_Count_the_Number_of_Computer_Unlocking_Permutations.py

--- Algorithms/Medium/3577_Count_the_Number_of_Computer_Unlocking_Permutations.py
+++ Algorithms/Medium/3577_Count_the_Number_of_Computer_Unlocking_Permutations.py
@@ -1,13 +1,13 @@
 from imports import *
 
+MOD, MAX_NUM = 10**9+7, 10**5
+FACT = {1: 1}
+for i in range(2, MAX_NUM+1):
+    FACT[i] = (FACT[i-1] * i) % MOD
+
 class Solution:
     def countPermutations(self, complexity: List[int]) -> int:
-        mod = 10**9 + 7
         if complexity.count(complexity[0]) > 1 or min(complexity) != complexity[0]:
             return 0
         
-        fact = 1
-        for i in range(2, len(complexity)):
-            fact = (fact * i) % mod
-
-        return fact
+        return FACT[len(complexity)-1]
