08fcf68dfd30a5759f7968e6c1a2ce49b5b04bf9
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-12-09 17:20:49 -0700

    Update 3583_Count_Special_Triplets.py

--- Algorithms/Medium/3583_Count_Special_Triplets.py
+++ Algorithms/Medium/3583_Count_Special_Triplets.py
@@ -2,19 +2,20 @@ from imports import *
 
 class Solution:
     def specialTriplets(self, nums: List[int]) -> int:
-        mod = 10**9 + 7
-        result = 0
-        n = len(nums)
-        
-        left, right = Counter(nums[ : 1]), Counter(nums[1 : ])
-        
-        for j in range(1, n-1):
-            right[nums[j]] -= 1
-            
-            curr = nums[j] * 2
-            if left[curr] > 0 and right[curr] > 0:
-                result = (result + (left[curr] * right[curr]) % mod) % mod
-            
-            left[nums[j]] += 1
+        result, mod, left, right = 0, 10**9+7, {}, {}
+        for num in nums:
+            if num % 2 == 0 and num//2 in right:
+                result = (result + right[num//2]) % mod
+
+            if num*2 in left:
+                if num in right:
+                    right[num] += left[num*2]
+                else:
+                    right[num] = left[num*2]
+                
+            if num in left:
+                left[num] += 1
+            else:
+                left[num] = 1
             
         return result
