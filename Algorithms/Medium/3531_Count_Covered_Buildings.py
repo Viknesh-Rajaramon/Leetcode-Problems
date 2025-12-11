6f6431a25a638934950d04016f9f0aa8f429dec6
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-12-10 17:29:50 -0700

    Update 3531_Count_Covered_Buildings.py

--- Algorithms/Medium/3531_Count_Covered_Buildings.py
+++ Algorithms/Medium/3531_Count_Covered_Buildings.py
@@ -2,18 +2,25 @@ from imports import *
 
 class Solution:
     def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
-        hash_x, hash_y = defaultdict(SortedList), defaultdict(SortedList)
+        min_row_indices, max_row_indices = [n+1] * (n+1), [0] * (n+1)
+        min_col_indices, max_col_indices = [n+1] * (n+1), [0] * (n+1)
 
-        for i, b in enumerate(buildings):
-            hash_x[b[0]].add((b[1], i))
-            hash_y[b[1]].add((b[0], i))
+        for row, col in buildings:
+            if row < min_row_indices[col]:
+                min_row_indices[col] = row
+            
+            if row > max_row_indices[col]:
+                max_row_indices[col] = row
+            
+            if col < min_col_indices[row]:
+                min_col_indices[row] = col
+            
+            if col > max_col_indices[row]:
+                max_col_indices[row] = col
+            
+        result = 0
+        for row, col in buildings:
+            if min_row_indices[col] < row < max_row_indices[col] and min_col_indices[row] < col < max_col_indices[row]:
+                result += 1
         
-        set_x = set()
-        for x, x_list in hash_x.items():
-            set_x.update([index for _, index in x_list[1:-1]])
-        
-        set_y = set()
-        for y, y_list in hash_y.items():
-            set_y.update([index for _, index in y_list[1:-1]])
-        
-        return len(set_x.intersection(set_y))
+        return result
