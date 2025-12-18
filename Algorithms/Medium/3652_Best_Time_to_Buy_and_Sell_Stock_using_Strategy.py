82046df1565f4f7ff26f41d4fd9d425c6e03bc1a
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-12-17 17:31:47 -0700

    Update 3652_Best_Time_to_Buy_and_Sell_Stock_using_Strategy.py

--- Algorithms/Medium/3652_Best_Time_to_Buy_and_Sell_Stock_using_Strategy.py
+++ Algorithms/Medium/3652_Best_Time_to_Buy_and_Sell_Stock_using_Strategy.py
@@ -1,26 +1,18 @@
-from imports import *
+from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int], strategy: List[int], k: int) -> int:
-        n, half = len(prices), k//2
+        result, n, half = [p*s for p, s in zip(prices, strategy)], len(prices), k//2
+        sum_a, sum_p = sum(result[ : k]), sum(prices[half : k])
+        best_delta = sum_p - sum_a
+        for l in range(1, n-k+1):
+            sum_a += result[l+k-1] - result[l-1]
+            sum_p += prices[l+k-1] - prices[l+half-1]
+            curr_delta = sum_p - sum_a
+            if curr_delta > best_delta:
+                best_delta = curr_delta
         
-        max_profit = 0
-        for i in range(n):
-            max_profit += prices[i] * strategy[i]
+        if best_delta < 0:
+            best_delta = 0
 
-        mod_profit = max_profit
-        for i in range(n):
-            if i-k >= 0:
-                mod_profit += prices[i-k] * strategy[i-k]
-                mod_profit -= prices[i-half]
-
-            if i < half:
-                mod_profit -= prices[i] * strategy[i]
-            else:
-                mod_profit -= prices[i] * strategy[i]
-                mod_profit += prices[i]
-            
-            if i-k >= -1:
-                max_profit = max(max_profit, mod_profit)
-
-        return max_profit
+        return sum(result) + best_delta
