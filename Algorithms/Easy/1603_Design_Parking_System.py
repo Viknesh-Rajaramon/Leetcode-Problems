74d4c4cd033d272edfbcfd7a58b6aff442a882ae
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-12-24 18:26:57 -0700

    Update 1603_Design_Parking_System.py

--- Algorithms/Easy/1603_Design_Parking_System.py
+++ Algorithms/Easy/1603_Design_Parking_System.py
@@ -1,18 +1,15 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.parking = {
-            1: big,
-            2: medium,
-            3: small
-        }
+        self.size = {1: big, 2: medium, 3: small}
 
     def addCar(self, carType: int) -> bool:
-        if self.parking[carType] > 0:
-            self.parking[carType] -= 1
-            return True
+        if self.size[carType] == 0:
+            return False
         
-        return False
+        self.size[carType] -= 1
+        return True
+
 
 # Your ParkingSystem object will be instantiated and called as such:
 # obj = ParkingSystem(big, medium, small)
