68640a56a808e2e6592a30d9162eefd574a5db24
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-12-25 21:17:30 -0700

    Update 94_Binary_Tree_Inorder_Traversal.py

--- Algorithms/Easy/94_Binary_Tree_Inorder_Traversal.py
+++ Algorithms/Easy/94_Binary_Tree_Inorder_Traversal.py
@@ -9,19 +9,7 @@ class TreeNode:
 
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        ptr = root
-        stack = []
-        arr = list()
-
-        while True:
-            if ptr is not None:
-                stack.append(ptr)
-                ptr = ptr.left
-            elif len(stack) != 0:
-                ptr = stack.pop()
-                arr.append(ptr.val)
-                ptr = ptr.right
-            else:
-                break
+        if not root:
+            return []
         
-        return arr
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
