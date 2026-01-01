b050d0bd0edd461f182a04cad8c379ee89e664be
Viknesh Rajaramon <viknesh0209@gmail.com>
2025-12-31 17:11:01 -0700

    Update 66_Plus_One.py

--- Algorithms/Easy/66_Plus_One.py
+++ Algorithms/Easy/66_Plus_One.py
@@ -6,6 +6,6 @@ class Solution:
         for i in range(len(digits)-1, -1, -1):
             new = digits[i] + carry
             digits[i] = new % 10
-            carry = new // 10
+            carry = 1 if new > 9 else 0
         
         return [1] + digits if carry == 1 else digits
