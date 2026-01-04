ecae235d84f24dca0d04ef303192c3bc343a4742
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-01-03 17:31:34 -0700

    Update 1390_Four_Divisors.py

--- Algorithms/Medium/1390_Four_Divisors.py
+++ Algorithms/Medium/1390_Four_Divisors.py
@@ -1,31 +1,19 @@
 from typing import List
-from collections import Counter
-from math import sqrt
 
 class Solution:
     def sumFourDivisors(self, nums: List[int]) -> int:
-        def sum_4_divisors(num: int) -> List[int]:
-            divisors = set()
-            
-            i = 1
-            while i <= sqrt(num):
-                if num % i == 0:
-                    divisors.add(i)
-                    divisors.add(num // i)
-
-                    if len(divisors) > 4:
-                        return 0
-                
-                i += 1
-            
-            if len(divisors) == 4:
-                return sum(divisors)
-            
-            return 0
-        
-        counts = Counter(nums)
         result = 0
-        for num, count in counts.items():
-            result += sum_4_divisors(num) * count
+        for num in nums:
+            s = set()
+            for i in range(1, int(num**0.5)+1):
+                if num%i == 0:
+                    s.add(i)
+                    s.add(num//i)
+                
+                    if len(s) > 4:
+                        break
+                
+            if len(s) == 4:
+                result += sum(s)
         
         return result
