7fdf97cdcd3339fc24a8c0adc66e2968abba3bd3
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-01-06 19:14:48 -0700

    Update 349_Intersection_of_Two_Arrays.py

--- Algorithms/Easy/349_Intersection_of_Two_Arrays.py
+++ Algorithms/Easy/349_Intersection_of_Two_Arrays.py
@@ -2,17 +2,4 @@ from typing import List
 
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans = []
-        dp = [0] * (1000 + 1)
-
-        n1 = len(nums1)
-        for i in range(n1):
-            dp[nums1[i]] += 1
-        
-        n2 = len(nums2)
-        for i in range(n2):
-            if dp[nums2[i]] > 0:
-                ans.append(nums2[i])
-                dp[nums2[i]] = 0
-        
-        return ans
+        return list(set(nums1).intersection(set(nums2)))
