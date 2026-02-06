fb79a0ab1c81ec229918b10719bf4c8599e98a4f
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-02-05 17:29:00 -0700

    Update 3634_Minimum_Removals_to_Balance_Array.py

--- Algorithms/Medium/3634_Minimum_Removals_to_Balance_Array.py
+++ Algorithms/Medium/3634_Minimum_Removals_to_Balance_Array.py
@@ -2,13 +2,10 @@ from typing import List
 
 class Solution:
     def minRemoval(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        n = len(nums)
-        i, count = 0, 0
-        for j in range(n):
-            while nums[j] > nums[i] * k:
-                i += 1
-            
-            count = max(count, j - i + 1)
+        nums.sort() 
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] > nums[l] * k:
+                l += 1
         
-        return n - count
+        return l
