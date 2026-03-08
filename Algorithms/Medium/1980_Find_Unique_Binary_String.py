090b9e52a732bb0c93ad732eb989652b4ddbe7ac
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-03-07 22:15:51 -0700

    Update 1980_Find_Unique_Binary_String.py

--- Algorithms/Medium/1980_Find_Unique_Binary_String.py
+++ Algorithms/Medium/1980_Find_Unique_Binary_String.py
@@ -3,22 +3,15 @@ from heapq import heapify, heappop
 
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        def convertNumToString(num: int, length: int) -> str:
-            digits = []
-            for _ in range(length):
-                digits.append(num % 2)
-                num = num // 2
-            
-            return "".join(str(i) for i in digits[::-1])
-
-        nums_int = [int(num, 2) for num in nums]
+        n, nums_int = len(nums[0]), [int(num, 2) for num in nums]
         heapify(nums_int)
-        
-        n = len(nums[0])
-        i = 0
-
         for i in range(2**n):
             if len(nums_int) == 0 or i != heappop(nums_int):
                 break
             
-        return convertNumToString(i, n)
+        result = []
+        for _ in range(n):
+            result.append(i % 2)
+            i = i // 2
+            
+        return "".join(str(i) for i in result[::-1])
