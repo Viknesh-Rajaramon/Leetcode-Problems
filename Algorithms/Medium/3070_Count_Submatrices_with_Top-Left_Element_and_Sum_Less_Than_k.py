6fb3864a3393a3597502951b8514b42ab8eeae8e
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-03-17 22:23:01 -0700

    Create 3070_Count_Submatrices_with_Top-Left_Element_and_Sum_Less_Than_k.py

---
+++ Algorithms/Medium/3070_Count_Submatrices_with_Top-Left_Element_and_Sum_Less_Than_k.py
@@ -0,0 +1,8 @@
+from typing import List
+from itertools import accumulate
+
+class Solution:
+    def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
+        grid = map(accumulate, grid)
+        grid = map(accumulate, zip(*grid))
+        return sum(val <= k for row in grid for val in row)
