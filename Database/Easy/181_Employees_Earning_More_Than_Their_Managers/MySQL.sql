8effc237f8deb66d0a1f7822b92b8e124bca4cfa
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-04-04 18:11:15 -0700

    Update MySQL.sql

--- Database/Easy/181_Employees_Earning_More_Than_Their_Managers/MySQL.sql
+++ Database/Easy/181_Employees_Earning_More_Than_Their_Managers/MySQL.sql
@@ -1,9 +1,8 @@
 -- Write your MySQL query statement below
-SELECT e.name AS Employee FROM Employee e
-INNER JOIN
-(
+SELECT e.name AS Employee FROM
+Employee e
+INNER JOIN (
   SELECT id, salary FROM Employee
 ) m
-ON
-e.managerId = m.id
+ON e.managerId = m.id
 WHERE e.salary > m.salary;
