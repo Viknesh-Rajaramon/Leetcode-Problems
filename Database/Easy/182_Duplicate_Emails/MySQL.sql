083cec3970e1b81ef3e09a8406447e0c318d550a
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-04-04 18:13:00 -0700

    Update MySQL.sql

--- Database/Easy/182_Duplicate_Emails/MySQL.sql
+++ Database/Easy/182_Duplicate_Emails/MySQL.sql
@@ -1,7 +1,5 @@
 -- Write your MySQL query statement below
-SELECT DISTINCT(p1.email) AS Email FROM
-Person p1
-INNER JOIN
-Person p2
-ON
-p1.email = p2.email AND p1.id != p2.id;
+SELECT p.email AS Email FROM
+Person p
+GROUP BY p.email
+HAVING COUNT(p.email) > 1;
