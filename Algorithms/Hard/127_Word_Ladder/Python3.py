c53c71af066d9cadf52c008f11207d92228a56c0
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-04-13 16:45:04 -0700

    change 127

--- Algorithms/Hard/127_Word_Ladder/Python3.py
+++ Algorithms/Hard/127_Word_Ladder/Python3.py
@@ -6,24 +6,20 @@ class Solution:
         if endWord not in wordList:
             return 0
         
-        n = len(beginWord)
-        
-        all_combo = defaultdict(list)
+        n, all_combo = len(beginWord), defaultdict(list)
         for word in wordList:
             for i in range(n):
                 all_combo[word[ : i] + "*" + word[i+1:]].append(word)
         
-        queue = deque([(beginWord, 1)])
-        visited = set([beginWord])
-        
+        queue, visited = deque([(beginWord, 1)]), set([beginWord])
         while queue:
             current_word, level = queue.popleft()
-
             for i in range(n):
                 intermediate_word = current_word[ : i] + "*" + current_word[i+1 : ]
                 for word in all_combo[intermediate_word]:
                     if word == endWord:
                         return level+1
+                    
                     if word not in visited:
                         queue.append((word, level+1))
                         visited.add(word)
