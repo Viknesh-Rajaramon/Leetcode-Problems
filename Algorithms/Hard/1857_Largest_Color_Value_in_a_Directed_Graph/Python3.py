14069038304c70aa93f8a2892ab5cbdec29e6f4c
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-04-14 11:58:06 -0700

    change 1857

--- Algorithms/Hard/1857_Largest_Color_Value_in_a_Directed_Graph/Python3.py
+++ Algorithms/Hard/1857_Largest_Color_Value_in_a_Directed_Graph/Python3.py
@@ -3,11 +3,8 @@ from collections import defaultdict, deque
 
 class Solution:
     def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
-        n = len(colors)
-        graph = defaultdict(list)
-        indegree = [0] * n
-        color_count = [defaultdict(int) for _ in range(n)]
-
+        n, graph = len(colors), defaultdict(list)
+        indegree, color_count = [0] * n, [defaultdict(int) for _ in range(n)]
         for u, v in edges:
             graph[u].append(v)
             indegree[v] += 1
@@ -17,13 +14,12 @@ class Solution:
             if indegree[i] == 0:
                 queue.append(i)
         
-        max_color_value = 0
+        result = 0
         while queue:
             u = queue.popleft()
 
             color_count[u][colors[u]] += 1
-            max_color_value = max(max_color_value, color_count[u][colors[u]])
-
+            result = max(result, color_count[u][colors[u]])
             for v in graph[u]:
                 for color, count in color_count[u].items():
                     color_count[v][color] = max(count, color_count[v][color])
@@ -36,4 +32,4 @@ class Solution:
             if indegree[i] != 0:
                 return -1
 
-        return max_color_value
+        return result
