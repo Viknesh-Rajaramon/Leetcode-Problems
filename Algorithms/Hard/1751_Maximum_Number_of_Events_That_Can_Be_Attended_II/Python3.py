96d9b47e9006d75a7a1589e437cdc22a92f29116
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-04-14 11:54:37 -0700

    change 1751

--- Algorithms/Hard/1751_Maximum_Number_of_Events_That_Can_Be_Attended_II/Python3.py
+++ Algorithms/Hard/1751_Maximum_Number_of_Events_That_Can_Be_Attended_II/Python3.py
@@ -16,12 +16,12 @@ class Solution:
             
             return low
         
-        nxt, prev = [binary_search(i+1, n) for i in range(n)], [0] * (n + 1)
+        nxt, result = [binary_search(i+1, n) for i in range(n)], [0] * (n + 1)
         for _ in range(k):
             curr = [0] * (n + 1)
             for i in range(n-1, -1, -1):
-                curr[i] = max(curr[i+1], events[i][2] + (prev[nxt[i]] if nxt[i] < n else 0))
+                curr[i] = max(curr[i+1], events[i][2] + (result[nxt[i]] if nxt[i] < n else 0))
             
-            prev = curr
+            result = curr
         
-        return prev[0]
+        return result[0]
