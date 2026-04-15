cc1f27672c9cb1ad9b807f1598289ead3194a7da
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-04-15 11:03:36 -0700

    change 3445

--- Algorithms/Hard/3445_Maximum_Difference_Between_Even_and_Odd_Frequency_II/Python3.py
+++ Algorithms/Hard/3445_Maximum_Difference_Between_Even_and_Odd_Frequency_II/Python3.py
@@ -2,33 +2,24 @@ from math import inf
 
 class Solution:
     def maxDifference(self, s: str, k: int) -> int:
-        n = len(s)
-        result = -inf
-
+        result, n = -inf, len(s)
         for a in "01234":
             for b in "01234":
                 if a == b:
                     continue
                 
-                count_a = [0] * (n+1)
-                count_b = [0] * (n+1)
-
+                count_a, count_b = [0] * (n+1), [0] * (n+1)
                 for i in range(n):
-                    count_a[i+1] = count_a[i] + (s[i] == a)
-                    count_b[i+1] = count_b[i] + (s[i] == b)
+                    count_a[i+1], count_b[i+1] = count_a[i]+(s[i] == a), count_b[i]+(s[i] == b)
                 
-                best_diff = [[-inf] * 2 for _ in range(2)]
-                l = 1
-                
+                best_diff, l = [[-inf] * 2 for _ in range(2)], 1
                 for r in range(k, n+1):
                     while r-l+1 >= k and count_a[r] > count_a[l-1] and count_b[r] > count_b[l-1]:
-                        pa = count_a[l-1] % 2
-                        pb = count_b[l-1] % 2
+                        pa, pb = count_a[l-1] % 2, count_b[l-1] % 2
                         best_diff[pa][pb] = max(best_diff[pa][pb], count_b[l-1] - count_a[l-1])
                         l += 1
 
-                    pa = count_a[r] % 2
-                    pb = count_b[r] % 2
+                    pa, pb = count_a[r] % 2, count_b[r] % 2
                     diff = best_diff[pa ^ 1][pb]
                     if diff != -inf:
                         result = max(result, count_a[r] - count_b[r] + diff)
