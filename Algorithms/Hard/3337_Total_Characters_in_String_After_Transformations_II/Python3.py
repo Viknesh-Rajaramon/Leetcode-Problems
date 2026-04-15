67d606c076baa3a6f4bcf450c02a1a6694aea8e1
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-04-15 10:15:44 -0700

    change 3337

--- Algorithms/Hard/3337_Total_Characters_in_String_After_Transformations_II/Python3.py
+++ Algorithms/Hard/3337_Total_Characters_in_String_After_Transformations_II/Python3.py
@@ -1,32 +1,28 @@
 from typing import List
 
-MOD = 10**9+7
-L = 26
-
 class Solution:
     def lengthAfterTransformations(self, s: str, t: int, nums: List[int]) -> int:
-        frequency = [0] * L
-
+        frequency, mod = [0] * 26, 10**9+7
         for c in s:
             frequency[ord(c) - ord("a")] += 1
         
-        matrix = [[0] * L for _ in range(L)]
-        for i in range(L):
+        matrix = [[0] * 26 for _ in range(26)]
+        for i in range(26):
             for j in range(1, nums[i] + 1):
-                matrix[(i+j) % L][i] = 1
+                matrix[(i+j) % 26][i] = 1
         
         def multiply(A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
-            C = [[0] * L for _ in range(L)]
-            for i in range(L):
-                for j in range(L):
-                    for k in range(L):
-                        C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % MOD
+            C = [[0] * 26 for _ in range(26)]
+            for i in range(26):
+                for j in range(26):
+                    for k in range(26):
+                        C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % mod
             
             return C
 
         def matrixExponentiation(matrix: List[List[int]], power: int) -> List[List[int]]:
-            ans = [[0] * L for _ in range(L)]
-            for i in range(L):
+            ans = [[0] * 26 for _ in range(26)]
+            for i in range(26):
                 ans[i][i] = 1
             
             x = matrix
@@ -39,11 +35,9 @@ class Solution:
             
             return ans
 
-        matrix = matrixExponentiation(matrix, t)
-
-        result = 0
-        for i in range(L):
-            for j in range(L):
-                result = (result + matrix[i][j] * frequency[j]) % MOD
+        result, matrix = 0, matrixExponentiation(matrix, t)
+        for i in range(26):
+            for j in range(26):
+                result = (result + matrix[i][j] * frequency[j]) % mod
             
         return result
