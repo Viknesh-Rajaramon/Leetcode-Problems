bbd043c43cfac25a7f63c98c525a13ce93ce10fe
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-04-15 10:11:15 -0700

    change 3333

--- Algorithms/Hard/3333_Find_the_Original_Typed_String_II/Python3.py
+++ Algorithms/Hard/3333_Find_the_Original_Typed_String_II/Python3.py
@@ -1,8 +1,6 @@
 class Solution:
     def possibleStringCount(self, word: str, k: int) -> int:
-        mod = 10**9 + 7
-        
-        groups, count = [], 1
+        mod, groups, count = 10**9 + 7, [], 1
         for i in range(1, len(word)):
             if word[i-1] == word[i]:
                 count += 1
@@ -23,14 +21,12 @@ class Solution:
         dp = [0] * k
         dp[0] = 1
         for num in groups:
-            new_dp = [0] * k
-            sum_ = 0
-
+            new_dp, sum_ = [0] * k, 0
             for j in range(k):
                 if j > 0:
                     sum_ = (sum_ + dp[j-1]) % mod
                 if j > num:
-                    sum_ = (sum_ - dp[j - num - 1] + mod) % mod
+                    sum_ = (sum_ - dp[j-num-1] + mod) % mod
                 
                 new_dp[j] = sum_
 
