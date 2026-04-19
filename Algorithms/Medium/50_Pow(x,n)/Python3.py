f0fbe959537cdfcea96ceeb24609fdd2a857fb17
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-04-19 12:05:06 -0700

    change medium

---
+++ Algorithms/Medium/50_Pow(x,n)/Python3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        elif n == 1:
+            return x
+        
+        if n < 0:
+            n, x = -n, 1/x
+        
+        pow_x = self.myPow(x, n//2)
+        if n % 2:
+            return pow_x * pow_x * x
+        
+        return pow_x * pow_x
