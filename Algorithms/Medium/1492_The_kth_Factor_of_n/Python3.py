eca5e55863e30dfb171867af9d0c6d6c778b082a
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-04-21 19:53:46 -0700

    Update Python3.py

--- Algorithms/Medium/1492_The_kth_Factor_of_n/Python3.py
+++ Algorithms/Medium/1492_The_kth_Factor_of_n/Python3.py
@@ -1,12 +1,9 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
-        count = 0
-
         for i in range(1, n+1):
             if n % i == 0:
-                count += 1
-            
-            if count == k:
-                return i
-
+                k -= 1
+                if k == 0:
+                    return i
+        
         return -1
