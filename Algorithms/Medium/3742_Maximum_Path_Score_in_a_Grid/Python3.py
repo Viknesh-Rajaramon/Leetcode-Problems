6d2c15c953c7ef64271df3a9c6f98620523aa66c
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-04-29 23:19:36 -0700

    Update Python3.py

--- Algorithms/Medium/3742_Maximum_Path_Score_in_a_Grid/Python3.py
+++ Algorithms/Medium/3742_Maximum_Path_Score_in_a_Grid/Python3.py
@@ -1,33 +1,25 @@
 from typing import List
-from math import inf
-from functools import lru_cache
 
 class Solution:
     def maxPathScore(self, grid: List[List[int]], k: int) -> int:
         m, n = len(grid), len(grid[0])
+        dp = [{} for _ in range(n)]
+        dp[0][k] = 0
 
-        @lru_cache(None)
-        def find(i: int, j: int, curr: int) -> int:
-            if i >= m or j >= n:
-                return -inf
-            
-            score, cost = 0, 0
-            if grid[i][j] == 1:
-                score, cost = 1, 1
-            
-            if grid[i][j] == 2:
-                score, cost = 2, 1
-            
-            if curr + cost > k:
-                return -inf
-            
-            if i == m-1 and j == n-1:
-                if curr <= k:
-                    return score
+        for i in range(m):
+            for j in range(n):
+                left, upper, res = dp[j-1].copy() if j > 0 else {}, dp[j].copy(), {}
+                for k, v in upper.items():
+                    if k not in left or left[k] < v:
+                        left[k] = v
+
+                if grid[i][j] != 0:
+                    for k, v in left.items():
+                        if k >= 1:
+                            res[k-1] = v + grid[i][j]
+                else:
+                    res = left
                 
-                return -inf
-            
-            return max(find(i+1, j, curr+cost), find(i, j+1, curr+cost)) + score
+                dp[j] = res
         
-        result = find(0, 0, 0)
-        return result if result != -inf else -1
+        return max(dp[n-1].values()) if len(dp[n-1]) != 0 else -1
