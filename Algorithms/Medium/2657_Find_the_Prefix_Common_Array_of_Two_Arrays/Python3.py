27eb841eef60224f64b714b1c47161849913657c
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-05-19 18:55:32 -0700

    Update Python3.py

--- Algorithms/Medium/2657_Find_the_Prefix_Common_Array_of_Two_Arrays/Python3.py
+++ Algorithms/Medium/2657_Find_the_Prefix_Common_Array_of_Two_Arrays/Python3.py
@@ -2,19 +2,18 @@ from typing import List
 
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-        n = len(A)
-        result, freq = [], [0] * n
-        curr = 0
-        for i in range(n):
-            freq[A[i] - 1] += 1
-            freq[B[i] - 1] += 1
+        result, seen, count = [], set(), 0
+        for a, b in zip(A, B):
+            if a in seen:
+                count += 1
+            else:
+                seen.add(a)
             
-            if freq[A[i] - 1] == 2:
-                curr += 1
+            if b in seen:
+                count += 1
+            else:
+                seen.add(b)
 
-            if A[i] != B[i] and freq[B[i] - 1] == 2:
-                curr += 1
-
-            result.append(curr)
+            result.append(count)
 
         return result
