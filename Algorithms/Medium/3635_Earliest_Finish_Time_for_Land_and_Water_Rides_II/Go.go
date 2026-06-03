38775f8b8003753593b21c6533dddaa6383c8da5
Viknesh Rajaramon <viknesh0209@gmail.com>
2026-06-02 17:24:24 -0700

    add 3635

---
+++ Algorithms/Medium/3635_Earliest_Finish_Time_for_Land_and_Water_Rides_II/Go.go
@@ -0,0 +1,19 @@
+func earliestFinishTime(landStartTime []int, landDuration []int, waterStartTime []int, waterDuration []int) int {
+    solve := func(firstStart, firstDur, secondStart, secondDur []int) int {
+        result, end := math.MaxInt, math.MaxInt
+        for i := range firstStart {
+            end = min(end, firstStart[i]+firstDur[i])
+        }
+        
+        for i := range secondStart {
+            result = min(result, max(secondStart[i], end)+secondDur[i])
+        }
+
+        return result
+    }
+
+    return min(
+        solve(landStartTime, landDuration, waterStartTime, waterDuration),
+        solve(waterStartTime, waterDuration, landStartTime, landDuration),
+    )
+}
--- Algorithms/Medium/3635_Earliest_Finish_Time_for_Land_and_Water_Rides_II/Python3.py
+++ Algorithms/Medium/3635_Earliest_Finish_Time_for_Land_and_Water_Rides_II/Python3.py
@@ -1,14 +1,12 @@
 from typing import List
-from math import inf
 
 class Solution:
     def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
         def solve(firstStart: List[int], firstDur: List[int], secondStart: List[int], secondDur: List[int]) -> int:
             end = min(s + d for s, d in zip(firstStart, firstDur))
-            result = inf
-            for s, d in zip(secondStart, secondDur):
-                result = min(result, max(s, end) + d)
-
-            return result
+            return min(max(s, end) + d for s, d in zip(secondStart, secondDur))
         
-        return min(solve(landStartTime, landDuration, waterStartTime, waterDuration), solve(waterStartTime, waterDuration, landStartTime, landDuration))
+        return min(
+            solve(landStartTime, landDuration, waterStartTime, waterDuration),
+            solve(waterStartTime, waterDuration, landStartTime, landDuration)
+        )
